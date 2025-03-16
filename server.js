cd9836b0e7d72af43b3ee3a28eee4b4e59459cde
Mark ALexis Batis <donclaine123@users.noreply.github.com>
2025-03-16 19:56:07 +0800

    fix

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const QRCode = require("qrcode");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 // Middleware
 app.use(cors());
