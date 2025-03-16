2911b8eb72d4c726a2559cdb2d700480b298c498
Mark ALexis Batis <donclaine123@users.noreply.github.com>
2025-03-16 19:48:29 +0800

    backend

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const bodyParser = require("body-parser");
 const QRCode = require("qrcode");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Middleware
 app.use(cors());
